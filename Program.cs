8b4e35e2698e4d00d23bdefbe984aad09b740ec2
Sarah <sarah.b.avram@gmail.com>
2020-09-21 20:47:42 -0400

    implemented Departments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
 
 namespace BestBuyBestPractices
 {
@@ -6,7 +11,27 @@ namespace BestBuyBestPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+            var repo = new DapperDepartmentRepository(conn);
+
+            Console.WriteLine("Type a new Department name.");
+
+            var newDepartment = Console.ReadLine();
+
+            repo.InsertDepartment(newDepartment);
+
+            var departments = repo.GetAllDepartments();
+
+            foreach (var dept in departments)
+            {
+                Console.WriteLine(dept.Name);
+            }
         }
     }
 }
